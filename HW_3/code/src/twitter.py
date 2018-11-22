0eceefd145baf4fd2b1698b481de333facd46bfc
Alex Gilbert <alexrgilbert@Alexs-MacBook-Air.local>
2018-11-21 20:26:21 -0800

    Converted to python3, and made sure imports worked etc...

--- HW_3/code/src/twitter.py
+++ HW_3/code/src/twitter.py
@@ -20,11 +20,11 @@ from sklearn import metrics
 def read_vector_file(fname):
     """
     Reads and returns a vector from a file.
-    
+
     Parameters
     --------------------
         fname  -- string, filename
-        
+
     Returns
     --------------------
         labels -- numpy array of shape (n,)
@@ -41,16 +41,16 @@ def extract_words(input_string):
     """
     Processes the input_string, separating it into "words" based on the presence
     of spaces, and separating punctuation marks into their own words.
-    
+
     Parameters
     --------------------
         input_string -- string of characters
-    
+
     Returns
     --------------------
         words        -- list of lowercase "words"
     """
-    
+
     for c in punctuation :
         input_string = input_string.replace(c, ' ' + c + ' ')
     return input_string.lower().split()
@@ -60,16 +60,16 @@ def extract_dictionary(infile):
     """
     Given a filename, reads the text file and builds a dictionary of unique
     words/punctuations.
-    
+
     Parameters
     --------------------
         infile    -- string, filename
-    
+
     Returns
     --------------------
         word_list -- dictionary, (key, value) pairs are (word, index)
     """
-    
+
     word_list = {}
     with open(infile, 'rU') as fid :
         ### ========== TODO : START ========== ###
@@ -84,12 +84,12 @@ def extract_feature_vectors(infile, word_list):
     """
     Produces a bag-of-words representation of a text file specified by the
     filename infile based on the dictionary word_list.
-    
+
     Parameters
     --------------------
         infile         -- string, filename
         word_list      -- dictionary, (key, value) pairs are (word, index)
-    
+
     Returns
     --------------------
         feature_matrix -- numpy array of shape (n,d)
@@ -97,17 +97,17 @@ def extract_feature_vectors(infile, word_list):
                             n is the number of non-blank lines in the text file
                             d is the number of unique words in the text file
     """
-    
+
     num_lines = sum(1 for line in open(infile,'rU'))
     num_words = len(word_list)
     feature_matrix = np.zeros((num_lines, num_words))
-    
+
     with open(infile, 'rU') as fid :
         ### ========== TODO : START ========== ###
         # part 1b: process each line to populate feature_matrix
         pass
         ### ========== TODO : END ========== ###
-        
+
     return feature_matrix
 
 
@@ -117,16 +117,16 @@ def extract_feature_vectors(infile, word_list):
 
 def performance(y_true, y_pred, metric="accuracy"):
     """
-    Calculates the performance metric based on the agreement between the 
+    Calculates the performance metric based on the agreement between the
     true labels and the predicted labels.
-    
+
     Parameters
     --------------------
         y_true -- numpy array of shape (n,), known labels
         y_pred -- numpy array of shape (n,), (continuous-valued) predictions
         metric -- string, option used to select the performance measure
-                  options: 'accuracy', 'f1-score', 'auroc'       
-    
+                  options: 'accuracy', 'f1-score', 'auroc'
+
     Returns
     --------------------
         score  -- float, performance score
@@ -134,7 +134,7 @@ def performance(y_true, y_pred, metric="accuracy"):
     # map continuous-valued predictions to binary labels
     y_label = np.sign(y_pred)
     y_label[y_label==0] = 1
-    
+
     ### ========== TODO : START ========== ###
     # part 2a: compute classifier performance
     return 0
@@ -147,7 +147,7 @@ def cv_performance(clf, X, y, kf, metric="accuracy"):
     Trains classifier on k-1 folds and tests on the remaining fold.
     Calculates the k-fold cross-validation performance metric for classifier
     by averaging the performance across folds.
-    
+
     Parameters
     --------------------
         clf    -- classifier (instance of SVC)
@@ -157,14 +157,14 @@ def cv_performance(clf, X, y, kf, metric="accuracy"):
         y      -- numpy array of shape (n,), binary labels {1,-1}
         kf     -- cross_validation.KFold or cross_validation.StratifiedKFold
         metric -- string, option used to select performance measure
-    
+
     Returns
     --------------------
         score   -- float, average cross-validation performance across k folds
     """
-    
+
     ### ========== TODO : START ========== ###
-    # part 2b: compute average cross-validation performance    
+    # part 2b: compute average cross-validation performance
     return 0
     ### ========== TODO : END ========== ###
 
@@ -174,7 +174,7 @@ def select_param_linear(X, y, kf, metric="accuracy"):
     Sweeps different settings for the hyperparameter of a linear-kernel SVM,
     calculating the k-fold CV performance for each setting, then selecting the
     hyperparameter that 'maximize' the average k-fold CV performance.
-    
+
     Parameters
     --------------------
         X      -- numpy array of shape (n,d), feature vectors
@@ -183,15 +183,15 @@ def select_param_linear(X, y, kf, metric="accuracy"):
         y      -- numpy array of shape (n,), binary labels {1,-1}
         kf     -- cross_validation.KFold or cross_validation.StratifiedKFold
         metric -- string, option used to select performance measure
-    
+
     Returns
     --------------------
         C -- float, optimal parameter value for linear-kernel SVM
     """
-    
-    print 'Linear SVM Hyperparameter Selection based on ' + str(metric) + ':'
+
+    print('Linear SVM Hyperparameter Selection based on ' + str(metric) + ':')
     C_range = 10.0 ** np.arange(-3, 3)
-    
+
     ### ========== TODO : START ========== ###
     # part 2: select optimal hyperparameter using cross-validation
     return 1.0
@@ -202,7 +202,7 @@ def select_param_linear(X, y, kf, metric="accuracy"):
 def performance_test(clf, X, y, metric="accuracy"):
     """
     Estimates the performance of the classifier using the 95% CI.
-    
+
     Parameters
     --------------------
         clf          -- classifier (instance of SVC)
@@ -212,7 +212,7 @@ def performance_test(clf, X, y, metric="accuracy"):
                           d = number of features
         y            -- numpy array of shape (n,), binary labels {1,-1} of test set
         metric       -- string, option used to select performance measure
-    
+
     Returns
     --------------------
         score        -- float, classifier performance
@@ -221,7 +221,7 @@ def performance_test(clf, X, y, metric="accuracy"):
     ### ========== TODO : START ========== ###
     # part 3: return performance on test data by first computing predictions and then calling performance
 
-    score = 0        
+    score = 0
     return score
     ### ========== TODO : END ========== ###
 
@@ -229,30 +229,29 @@ def performance_test(clf, X, y, metric="accuracy"):
 ######################################################################
 # main
 ######################################################################
- 
+
 def main() :
     np.random.seed(1234)
-    
-    # read the tweets and its labels   
+
+    # read the tweets and its labels
     dictionary = extract_dictionary('../data/tweets.txt')
     X = extract_feature_vectors('../data/tweets.txt', dictionary)
     y = read_vector_file('../data/labels.txt')
-    
+
     metric_list = ["accuracy", "f1_score", "auroc"]
-    
     ### ========== TODO : START ========== ###
     # part 1: split data into training (training + cross-validation) and testing set
-    
+
     # part 2: create stratified folds (5-fold CV)
-    
+
     # part 2: for each metric, select optimal hyperparameter for linear-kernel SVM using CV
-        
+
     # part 3: train linear-kernel SVMs with selected hyperparameters
-    
+
     # part 3: report performance on test data
-    
+
     ### ========== TODO : END ========== ###
-    
-    
+
+
 if __name__ == "__main__" :
     main()
